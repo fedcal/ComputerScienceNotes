ce6212dd6bd7a41e20c38caa871c4e82e2c9f4f5
Federico Calo <fedcal01@gmail.com>
2022-08-22 12:49:38 +0200

    font awesome icons

---
+++ src/components/navbar/SidebarData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+export const SidebarData = [
+    {
+        title:'Il progetto',
+        path: '/',
+        icon:  <FontAwesomeIcon icon="fa-solid fa-house" />,
+        cName: 'nav-Text'
+    },
+    {
+        title:'Metodo avanzati di programmazione',
+        path: '/',
+        icon: <FontAwesomeIcon icon="fa-solid fa-brackets-curly" />,
+        cName: 'nav-Text'
+    },
+    {
+        title:'Ingegneria della conoscienza',
+        path: '/',
+        icon: <FontAwesomeIcon icon="fa-solid fa-books" />,
+        cName: 'nav-Text'
+    },
+    {
+        title:'Ingegneria del software',
+        path: '/',
+        icon: <FontAwesomeIcon icon="fa-solid fa-laptop-code" />,
+        cName: 'nav-Text'
+    }
+    
+]
